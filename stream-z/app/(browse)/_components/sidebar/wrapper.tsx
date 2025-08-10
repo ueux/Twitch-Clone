856c82b67c9b656a614aa3914f61ba8b12e0fc8f
Harsh Kumar <hemenderkumar3000@gmail.com>
2025-08-11 05:23:22 +0530

    Recommendations

--- stream-z/app/(browse)/_components/sidebar/wrapper.tsx
+++ stream-z/app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,13 +2,25 @@
 
 import { cn } from "@/lib/utils"
 import { useSidebar } from "@/store/use-sidebar"
+import { useEffect, useState } from "react"
+import { ToggleSkeleton } from "./toggle"
+import { RecommendedSkeleton } from "./recommended"
+import { useIsClient } from "usehooks-ts"
 
 interface WrapperProps {
     children: React.ReactNode
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+    const isClient=useIsClient()
     const { collapsed } = useSidebar((state) => state)
+
+    if (!isClient) return (
+        <aside className={"fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-[#252731] border-r border-[#2D2E35] z-50"}>
+            <ToggleSkeleton />
+            <RecommendedSkeleton/>
+        </aside>
+    )
 
     return (<aside className={cn("fixed left-0 flex flex-col w-60 h-full bg-[#252731] border-r border-[#2D2E35] z-50",
         collapsed&&"w-[70px]"
